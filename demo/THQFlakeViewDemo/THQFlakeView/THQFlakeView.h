d164044db60263408960cc417985fce92e7cd800
zhaoqing <985344727@qq.com>
2017-01-03 18:29:10 +0800

    删除多余代码

--- demo/THQFlakeViewDemo/THQFlakeView/THQFlakeView.h
+++ demo/THQFlakeViewDemo/THQFlakeView/THQFlakeView.h
@@ -9,8 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "THQFlakeImageView.h"
 
-static const NSString *THQFlakeViewappWillEnterForegroundNotification = @"AppWillEnterForegroundNotification";
-
 @interface THQFlakeView : THQFlakeImageView
 
 // 特定的下雪花的控制器，THQFlakeView不一定是加在这个控制器上的，也有可能是加在UINavigationController上
